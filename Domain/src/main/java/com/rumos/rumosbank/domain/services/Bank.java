851776df695468ff0fa46387ed6827e8c65d70a5
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-02-28 23:28:45 +0000

    added a new method to update the movements of an account

--- Domain/src/main/java/com/rumos/rumosbank/domain/services/Bank.java
+++ Domain/src/main/java/com/rumos/rumosbank/domain/services/Bank.java
@@ -1,11 +1,20 @@
 package com.rumos.rumosbank.domain.services;
 
+import com.rumos.rumosbank.domain.models.BankAccount;
 import com.rumos.rumosbank.domain.models.Client;
 import com.rumos.rumosbank.domain.repositories.ClientRepository;
+import com.rumos.rumosbank.domain.repositories.DepositRepository;
+import com.rumos.rumosbank.domain.repositories.TransferRepository;
+import com.rumos.rumosbank.domain.repositories.WithdrawRepository;
+
+import javax.naming.OperationNotSupportedException;
 
 public class Bank {
     public static final Bank instance;
     static { instance = new Bank(); }
+
+
+    /* ------------------------------------------------------------ Authentication ------------------------------------------------------------ */
 
     public Client authenticate(String email, String password) {
         Client client = new ClientRepository().get(email);
@@ -15,11 +24,23 @@ public class Bank {
         return null;
     }
 
+    /* ------------------------------------------------------------ Clients ------------------------------------------------------------ */
+
     public void registerNewClient(Client client) {
         new ClientRepository().insert(client);
     }
 
     public void updateClient(Client client) {
         new ClientRepository().update(client);
+    }
+
+    /* ------------------------------------------------------------ Movements ------------------------------------------------------------ */
+
+    public void updateMovements(BankAccount bankAccount) throws OperationNotSupportedException {
+        long bankAccountId = bankAccount.getId();
+        bankAccount.setMovements(new WithdrawRepository().get(bankAccountId),
+                                 new DepositRepository().get(bankAccountId),
+                                 new TransferRepository().getSent(bankAccountId),
+                                 new TransferRepository().getReceived(bankAccountId));
     }
 }
