b19d72a293cb2929100cf4fa7f17549849645525
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-02-28 21:15:53 +0000

    added mapped super class notation so the withdraw and deposit classes can reference the bank account field

--- Domain/src/main/java/com/rumos/rumosbank/domain/models/movements/Operation.java
+++ Domain/src/main/java/com/rumos/rumosbank/domain/models/movements/Operation.java
@@ -3,7 +3,9 @@ package com.rumos.rumosbank.domain.models.movements;
 import com.rumos.rumosbank.domain.models.BankAccount;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MappedSuperclass;
 
+@MappedSuperclass
 public abstract class Operation extends Movement {
 
     @ManyToOne
