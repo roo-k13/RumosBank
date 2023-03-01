eb0b7bf9a2a07c5e9111e55c62b00f3d54bd7e23
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-01 16:10:01 +0000

    added a method to update the account balance

--- ATMApp/src/main/java/com/rumos/rumosbank/atmapp/controllers/MenuController.java
+++ ATMApp/src/main/java/com/rumos/rumosbank/atmapp/controllers/MenuController.java
@@ -7,41 +7,48 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 import java.io.IOException;
+import java.text.NumberFormat;
 
 public class MenuController {
 
     @FXML
     private Label account_balance_label;
     @FXML
-    private void initialize() { OperationsController.setAccountBalance(account_balance_label); }
+    private void initialize() { setAccountBalance(); }
 
     /* ------------------------------------------------------------ Options ------------------------------------------------------------ */
 
+    private void setAccountBalance() {
+        NumberFormat formatter = NumberFormat.getCurrencyInstance();
+        String balance = formatter.format(App.authenticatedAccount.getBalance());
+        account_balance_label.setText("Balance: " + balance);
+    }
+
     @FXML
-    private void onWithdrawButtonClick(ActionEvent event) {
+    private void onWithdrawButtonClick(ActionEvent actionEvent) {
         OperationsController.selectedOperation = "Withdraw";
-        try { App.changeScene(event, "/fxml/operation.fxml"); }
+        try { App.changeScene(actionEvent, "/fxml/operation.fxml"); }
         catch (IOException exception) { throw new RuntimeException(exception); }
     }
 
     @FXML
-    private void onDepositButtonClick(ActionEvent event) {
+    private void onDepositButtonClick(ActionEvent actionEvent) {
         OperationsController.selectedOperation = "Deposit";
-        try { App.changeScene(event, "/fxml/operation.fxml"); }
+        try { App.changeScene(actionEvent, "/fxml/operation.fxml"); }
         catch (IOException exception) { throw new RuntimeException(exception); }
     }
 
     @FXML
-    private void onChangePinButtonClick(ActionEvent event) {
-        try { App.changeScene(event, "/fxml/change_pin.fxml"); }
+    private void onChangePinButtonClick(ActionEvent actionEvent) {
+        try { App.changeScene(actionEvent, "/fxml/change_pin.fxml"); }
         catch (IOException exception) { throw new RuntimeException(exception); }
     }
 
     @FXML
-    private void onLogoutButtonClick(ActionEvent event) {
+    private void onLogoutButtonClick(ActionEvent actionEvent) {
         App.authenticatedAccount = null;
         App.authenticatedCard = null;
-        try { App.changeScene(event, "/fxml/login.fxml"); }
+        try { App.changeScene(actionEvent, "/fxml/login.fxml"); }
         catch (IOException exception) { throw new RuntimeException(exception); }
     }
 }
