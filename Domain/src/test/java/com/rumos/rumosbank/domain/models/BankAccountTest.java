040d41f0496a9b440168548125e88e62b969ff44
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-02 11:37:46 +0000

    removed unnecessary import

--- Domain/src/test/java/com/rumos/rumosbank/domain/models/BankAccountTest.java
+++ Domain/src/test/java/com/rumos/rumosbank/domain/models/BankAccountTest.java
@@ -1,6 +1,5 @@
 package com.rumos.rumosbank.domain.models;
 
-import com.rumos.rumosbank.domain.models.BankAccount;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
