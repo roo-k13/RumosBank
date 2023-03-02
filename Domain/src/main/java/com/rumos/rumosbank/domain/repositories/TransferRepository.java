186aa833513d5d3b4aaf5c3bedb0a00d8ea18168
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-02 11:31:05 +0000

    created a new method to get all the sent transfers

--- Domain/src/main/java/com/rumos/rumosbank/domain/repositories/TransferRepository.java
+++ Domain/src/main/java/com/rumos/rumosbank/domain/repositories/TransferRepository.java
@@ -2,12 +2,28 @@ package com.rumos.rumosbank.domain.repositories;
 
 import com.rumos.rumosbank.domain.models.movements.Transfer;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 import javax.naming.OperationNotSupportedException;
 import java.util.List;
 
 public class TransferRepository {
+    private final EntityManager entityManager;
+    private final EntityManagerFactory entityManagerFactory;
+
+    public TransferRepository() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("default");
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
     public List<Transfer> getSent(long bankAccountId) throws OperationNotSupportedException {
-        throw new OperationNotSupportedException();
+        String query = "SELECT t FROM Transfer t WHERE t.sender.id = :accountId";
+        return entityManager
+                .createQuery(query, Transfer.class)
+                .setParameter("bankaccountId", bankAccountId)
+                .getResultList();
     }
 
     public List<Transfer> getReceived(long bankAccountId) throws OperationNotSupportedException {
