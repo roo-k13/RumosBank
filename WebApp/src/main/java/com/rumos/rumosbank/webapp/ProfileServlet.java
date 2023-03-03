5f2fbce6fabb611168c6b969b27d66a242719aec
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-03 10:16:46 +0000

    added a doPost() method to update the client when he edits his profile

--- WebApp/src/main/java/com/rumos/rumosbank/webapp/ProfileServlet.java
+++ WebApp/src/main/java/com/rumos/rumosbank/webapp/ProfileServlet.java
@@ -1,5 +1,7 @@
 package com.rumos.rumosbank.webapp;
 
+import com.rumos.rumosbank.domain.models.Client;
+import com.rumos.rumosbank.domain.services.Bank;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -15,8 +17,14 @@ public class ProfileServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession();
         RequestDispatcher dispatcher = request.getRequestDispatcher("profile.jsp");
         dispatcher.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        Client client = (Client) session.getAttribute("client");
+        Bank.instance.updateClient(client);
     }
 }
