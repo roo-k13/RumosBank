2924e2df56b059c15c338812eea290aedb8749aa
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-03 20:01:20 +0000

    added a new method to get bank accounts by their number

--- Domain/src/main/java/com/rumos/rumosbank/domain/repositories/BankAccountRepository.java
+++ Domain/src/main/java/com/rumos/rumosbank/domain/repositories/BankAccountRepository.java
@@ -21,4 +21,9 @@ public class BankAccountRepository {
         entityManager.close();
         entityManagerFactory.close();
     }
+
+    public BankAccount get(String number) {
+        String query = "SELECT b FROM BankAccount b WHERE b.number = :number";
+        return entityManager.createQuery(query, BankAccount.class).setParameter("number", number).getSingleResult();
+    }
 }
