aba0fb520044b0812946be0241293c5b0b733bbf
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-03 22:30:11 +0000

    added new fields to the form and created a class property that points to the authenticatedClient in the app

--- BankApp/src/main/java/com/rumos/rumosbank/bankapp/controllers/UpdateInfoController.java
+++ BankApp/src/main/java/com/rumos/rumosbank/bankapp/controllers/UpdateInfoController.java
@@ -11,6 +11,8 @@ import javafx.scene.control.TextField;
 import java.io.IOException;
 
 public class UpdateInfoController {
+    private Client client;
+
     @FXML
     private TextField first_name_text_field;
     @FXML
@@ -20,24 +22,65 @@ public class UpdateInfoController {
     @FXML
     private TextField nif_text_field;
     @FXML
+    private TextField phone_text_field;
+    @FXML
+    private TextField mobile_phone_text_field;
+    @FXML
+    private TextField profession_text_field;
+    @FXML
     private TextField email_text_field;
 
     @FXML
-    void initialize() {
-        Client client = App.getAuthenticatedClient();
+    private void initialize() {
+        client = App.getAuthenticatedClient();
+        initializeFields();
+
+    }
+
+    /* ----------------------------------------------------- Navigation Bar ----------------------------------------------------- */
+
+    @FXML
+    private void onAccountsButtonClick(ActionEvent actionEvent) {
+        NavigationBarController.accounts(actionEvent);
+    }
+
+    @FXML
+    private void onEditProfileButtonClick(ActionEvent actionEvent) {
+        NavigationBarController.profile(actionEvent);
+    }
+
+    @FXML
+    private void onChangePasswordButtonClick(ActionEvent actionEvent) {
+        NavigationBarController.password(actionEvent);
+    }
+
+    @FXML
+    private void onLogoutButtonClick(ActionEvent actionEvent) {
+        NavigationBarController.logout(actionEvent);
+    }
+
+    /* ---------------------------------------------------------- Form ---------------------------------------------------------- */
+
+    private void initializeFields() {
         first_name_text_field.setText(client.getFirstName());
         last_name_text_field.setText(client.getLastName());
         birthdate_text_field.setText(client.getBirthdate().toString());
         nif_text_field.setText(client.getNif());
+        phone_text_field.setText(client.getPhone());
+        mobile_phone_text_field.setText(client.getMobilePhone());
+        profession_text_field.setText(client.getProfession());
         email_text_field.setText(client.getEmailAddress());
     }
 
     @FXML
-    protected void onSaveChangesButtonClick(ActionEvent actionEvent) {
-        App.getAuthenticatedClient().setFirstName(first_name_text_field.getText());
-        App.getAuthenticatedClient().setLastName(last_name_text_field.getText());
-        App.getAuthenticatedClient().setEmailAddress(email_text_field.getText());
-        Bank.instance.updateClient(App.getAuthenticatedClient());
+    private void onSaveChangesButtonClick(ActionEvent actionEvent) {
+        client.setFirstName(first_name_text_field.getText());
+        client.setLastName(last_name_text_field.getText());
+        client.setPhone(phone_text_field.getText());
+        client.setMobilePhone(mobile_phone_text_field.getText());
+        client.setProfession(profession_text_field.getText());
+        client.setEmailAddress(email_text_field.getText());
+        Bank.instance.updateClient(client);
         try { App.changeScene(actionEvent, "/fxml/login.fxml"); }
         catch (IOException exception) { throw new RuntimeException(exception); }
     }
