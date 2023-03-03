cded2514696c892eeede0b5a45f25089f5cd8185
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-03 22:18:11 +0000

    added setters for the sender and receiver bank account

--- Domain/src/main/java/com/rumos/rumosbank/domain/models/movements/Transfer.java
+++ Domain/src/main/java/com/rumos/rumosbank/domain/models/movements/Transfer.java
@@ -1,6 +1,7 @@
 package com.rumos.rumosbank.domain.models.movements;
 
 import com.rumos.rumosbank.domain.models.BankAccount;
+
 import jakarta.persistence.*;
 
 @Entity
@@ -10,4 +11,8 @@ public class Transfer extends Movement {
     private BankAccount sender;
     @ManyToOne
     private BankAccount receiver;
+
+    public void setSender(BankAccount sender) { this.sender = sender; }
+
+    public void setReceiver(BankAccount receiver) { this.receiver = receiver; }
 }
