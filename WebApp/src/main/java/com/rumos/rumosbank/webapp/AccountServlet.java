438ae5a98d57f2b35cb1279aa68cfe6753e41c1f
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-03 09:43:50 +0000

    created a new servlet to control the individual account jsp

---
+++ WebApp/src/main/java/com/rumos/rumosbank/webapp/AccountServlet.java
@@ -0,0 +1,26 @@
+package com.rumos.rumosbank.webapp;
+
+import com.rumos.rumosbank.domain.models.BankAccount;
+import com.rumos.rumosbank.domain.models.Client;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "account", value = "/account")
+public class AccountServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String number = request.getParameter("number");
+        Client client = (Client) request.getSession().getAttribute("client");
+        BankAccount bankAccount = client.getBankAccount(number);
+        request.getSession().setAttribute("account", bankAccount);
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("account.jsp");
+        requestDispatcher.forward(request, response);
+    }
+}
