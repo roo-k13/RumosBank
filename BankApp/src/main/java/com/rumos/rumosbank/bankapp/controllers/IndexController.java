b429d7dc2b9afe46ee13b03031d4d693384a054a
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-04 18:01:22 +0000

    this class now extends the navigation bar controller

--- BankApp/src/main/java/com/rumos/rumosbank/bankapp/controllers/IndexController.java
+++ BankApp/src/main/java/com/rumos/rumosbank/bankapp/controllers/IndexController.java
@@ -3,9 +3,5 @@ package com.rumos.rumosbank.bankapp.controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
-public class IndexController {
-    @FXML
-    private void onLogoutButtonClick(ActionEvent actionEvent) {
-        NavigationBarController.logout(actionEvent);
-    }
+public class IndexController extends NavigationBarController {
 }
