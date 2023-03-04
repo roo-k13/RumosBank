c1bee9e38b6198088eb39fac904f662ab0033102
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-04 18:09:17 +0000

    added the properties of the cards table and respective imports

--- BankApp/src/main/java/com/rumos/rumosbank/bankapp/controllers/CardsController.java
+++ BankApp/src/main/java/com/rumos/rumosbank/bankapp/controllers/CardsController.java
@@ -1,11 +1,24 @@
 package com.rumos.rumosbank.bankapp.controllers;
 
 import com.rumos.rumosbank.domain.models.BankAccount;
+import com.rumos.rumosbank.domain.models.cards.Card;
 import com.rumos.rumosbank.domain.services.Bank;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class CardsController extends NavigationBarController{
+
+    @FXML
+    private ChoiceBox<BankAccount> bank_accounts_choice_box;
+    @FXML
+    private TableView<Card> accounts_table_view;
+    @FXML
+    private TableColumn<Card, String> card_type;
+    @FXML
+    private TableColumn<Card, String> card_date;
 
     @FXML
     private void onCreateNewDebitCardButtonClick() {
