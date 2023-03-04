6a96b852ca77caac0d6e2c88238923e96c5638a2
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-04 18:05:25 +0000

    organized imports and extends navigation bar controller

--- BankApp/src/main/java/com/rumos/rumosbank/bankapp/controllers/CardsController.java
+++ BankApp/src/main/java/com/rumos/rumosbank/bankapp/controllers/CardsController.java
@@ -2,9 +2,11 @@ package com.rumos.rumosbank.bankapp.controllers;
 
 import com.rumos.rumosbank.domain.models.BankAccount;
 import com.rumos.rumosbank.domain.services.Bank;
+
 import javafx.fxml.FXML;
 
-public class CardsController {
+public class CardsController extends NavigationBarController{
+
     @FXML
     private void onCreateNewDebitCardButtonClick() {
         BankAccount bankAccount = new BankAccount();
