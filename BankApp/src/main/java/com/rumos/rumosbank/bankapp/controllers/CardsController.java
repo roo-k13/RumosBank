c031a6243214d83560df80750657a146a11b2e21
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-07 13:53:20 +0000

    completed the methos to fill the debit and credit cards lists when a bank account is chosen

--- BankApp/src/main/java/com/rumos/rumosbank/bankapp/controllers/CardsController.java
+++ BankApp/src/main/java/com/rumos/rumosbank/bankapp/controllers/CardsController.java
@@ -1,29 +1,73 @@
 package com.rumos.rumosbank.bankapp.controllers;
 
+import com.rumos.rumosbank.bankapp.App;
 import com.rumos.rumosbank.domain.models.BankAccount;
-import com.rumos.rumosbank.domain.models.cards.Card;
+import com.rumos.rumosbank.domain.models.cards.CreditCard;
+import com.rumos.rumosbank.domain.models.cards.DebitCard;
 import com.rumos.rumosbank.domain.services.Bank;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.ListView;
 
-public class CardsController extends NavigationBarController{
+import java.util.List;
+
+public class CardsController extends NavigationBarController {
+    private BankAccount selectedAccount;
+
+    @FXML private void initialize() {
+        initializeAccountsChoiceBox();
+    }
+
+    /* --------------------------------------------------------- Accounts Choice Box ------------------------------------------------------- */
 
     @FXML
     private ChoiceBox<BankAccount> bank_accounts_choice_box;
+
     @FXML
-    private TableView<Card> accounts_table_view;
+    private void initializeAccountsChoiceBox() {
+        bank_accounts_choice_box.setOnAction((this::onSelectingBankAccount));
+        List<BankAccount> bankAccounts = App.getAuthenticatedClient().getBankAccounts();
+        bank_accounts_choice_box.setItems(FXCollections.observableList(bankAccounts));
+    }
+
+    private void onSelectingBankAccount(ActionEvent actionEvent) {
+        selectedAccount = bank_accounts_choice_box.getValue();
+        updateCards();
+    }
+
+    private void updateCards() {
+        updateDebitCards();
+        updateCreditCards();
+    }
+
+    /* --------------------------------------------------------- Debit Cards------------------------------------------------------- */
+
     @FXML
-    private TableColumn<Card, String> card_type;
-    @FXML
-    private TableColumn<Card, String> card_date;
+    private ListView<DebitCard> debit_cards_list_view;
+
+    private void updateDebitCards() {
+        ObservableList<DebitCard> debitCards = FXCollections.observableArrayList(selectedAccount.getDebitCards());
+        debit_cards_list_view.setItems(debitCards);
+    }
 
     @FXML
     private void onCreateNewDebitCardButtonClick() {
         BankAccount bankAccount = new BankAccount();
         Bank.instance.createNewDebitCard(bankAccount);
+    }
+
+    /* --------------------------------------------------------- Credit Cards------------------------------------------------------- */
+
+    @FXML
+    private ListView<CreditCard> credit_cards_list_view;
+
+    private void updateCreditCards() {
+        ObservableList<CreditCard> creditCards = FXCollections.observableArrayList(selectedAccount.getCreditCards());
+        credit_cards_list_view.setItems(creditCards);
     }
 
     @FXML
