ae2f4430128348c259790ef94e3aeaef73cfb4fa
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-07 15:07:57 +0000

    organized imports, extended the navigation bar controller, changed the password fields from text field to password fields, created an array for the password fields and modified the onRegisterButtonClick() method to now update the nif, phone, mobile phone and profession

--- BankApp/src/main/java/com/rumos/rumosbank/bankapp/controllers/RegistrationController.java
+++ BankApp/src/main/java/com/rumos/rumosbank/bankapp/controllers/RegistrationController.java
@@ -2,12 +2,14 @@ package com.rumos.rumosbank.bankapp.controllers;
 
 import com.rumos.rumosbank.domain.models.Client;
 import com.rumos.rumosbank.domain.services.Bank;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
-public class RegistrationController {
+public class RegistrationController extends NavigationBarController {
 
     @FXML
     private TextField first_name_text_field;
@@ -26,20 +28,22 @@ public class RegistrationController {
     @FXML
     public TextField profession_text_field;
     @FXML
-    private TextField insert_password_text_field;
+    private PasswordField insert_password_password_field;
     @FXML
-    private TextField confirm_password_text_field;
+    private PasswordField confirm_password_password_field;
     @FXML
     private Label feedback_message_label;
 
     private TextField[] textFields;
+    private PasswordField[] passwordFields;
 
     @FXML
     private void initialize() {
         textFields = new TextField[] {
-                first_name_text_field, last_name_text_field, email_text_field,
-                insert_password_text_field, confirm_password_text_field
+                first_name_text_field, last_name_text_field, email_text_field, nif_text_field, phone_text_field,
+                mobile_phone_text_field, profession_text_field,
         };
+        passwordFields = new PasswordField[] { insert_password_password_field, confirm_password_password_field};
         feedback_message_label.setVisible(false);
     }
 
@@ -61,8 +65,12 @@ public class RegistrationController {
             client.setFirstName(first_name_text_field.getText());
             client.setLastName(last_name_text_field.getText());
             client.setBirthdate(birthdate_date_picker.getValue());
+            client.setNif(nif_text_field.getText());
             client.setEmailAddress(email_text_field.getText());
-            client.setPassword(insert_password_text_field.getText());
+            client.setPhone(phone_text_field.getText());
+            client.setMobilePhone(mobile_phone_text_field.getText());
+            client.setProfession(profession_text_field.getText());
+            client.setPassword(insert_password_password_field.getText());
             new Bank().registerNewClient(client);
         }
     }
