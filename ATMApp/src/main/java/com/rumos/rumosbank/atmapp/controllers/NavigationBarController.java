d8e2e8e0ae8e6b8b5e141614e664c8143ce993f1
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-11 09:59:10 +0000

    class is no longer abstract

--- ATMApp/src/main/java/com/rumos/rumosbank/atmapp/controllers/NavigationBarController.java
+++ ATMApp/src/main/java/com/rumos/rumosbank/atmapp/controllers/NavigationBarController.java
@@ -6,14 +6,7 @@ import javafx.fxml.FXML;
 
 import java.io.IOException;
 
-public abstract class NavigationBarController {
-
-    protected static void logout(ActionEvent actionEvent) {
-        App.setAuthenticatedAccount(null);
-        App.setAuthenticatedCard(null);
-        try { App.changeScene(actionEvent, "/fxml/login.fxml"); }
-        catch (IOException exception) { throw new RuntimeException(exception); }
-    }
+public class NavigationBarController {
 
     @FXML
     private void onWithdrawButtonClick(ActionEvent actionEvent) {
@@ -32,6 +25,14 @@ public abstract class NavigationBarController {
     @FXML
     private void onChangePinButtonClick(ActionEvent actionEvent) {
         try { App.changeScene(actionEvent, "/fxml/change_pin.fxml"); }
+        catch (IOException exception) { throw new RuntimeException(exception); }
+    }
+
+    @FXML
+    protected void onLogoutButtonClick(ActionEvent actionEvent) {
+        App.setAuthenticatedAccount(null);
+        App.setAuthenticatedCard(null);
+        try { App.changeScene(actionEvent, "/fxml/login.fxml"); }
         catch (IOException exception) { throw new RuntimeException(exception); }
     }
 }
