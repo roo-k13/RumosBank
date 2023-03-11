1deb43f5616d66675034efadc098b712c0205b0b
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-11 22:58:43 +0000

    refactored the methods

--- BankApp/src/main/java/com/rumos/rumosbank/bankapp/controllers/UpdateInfoController.java
+++ BankApp/src/main/java/com/rumos/rumosbank/bankapp/controllers/UpdateInfoController.java
@@ -4,68 +4,73 @@ import com.rumos.rumosbank.bankapp.App;
 import com.rumos.rumosbank.domain.models.Client;
 import com.rumos.rumosbank.domain.services.Bank;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-import java.io.IOException;
-
 public class UpdateInfoController extends NavigationBarController {
-    private Client client;
+    private final Bank bank;
+    private final Client client;
 
     @FXML
-    private TextField first_name_text_field;
+    private TextField firstNameTextField;
     @FXML
-    private TextField last_name_text_field;
+    private TextField lastNameTextField;
     @FXML
-    private TextField birthdate_text_field;
+    private TextField birthdateTextField;
     @FXML
-    private TextField nif_text_field;
+    private TextField nifTextField;
     @FXML
-    private TextField phone_text_field;
+    private TextField phoneTextField;
     @FXML
-    private TextField mobile_phone_text_field;
+    private TextField mobilePhoneTextField;
     @FXML
-    private TextField profession_text_field;
+    private TextField professionTextField;
     @FXML
-    private TextField email_text_field;
+    private TextField emailTextField;
     @FXML
-    private Label feedback_message_label;
+    private Label feedbackMessageLabel;
+
+    public UpdateInfoController() {
+        this.bank = Bank.instance;
+        this.client = App.getAuthenticatedClient();
+    }
 
     @FXML
     private void initialize() {
-        client = App.getAuthenticatedClient();
         initializeFields();
-        feedback_message_label.setVisible(false);
-
+        feedbackMessageLabel.setVisible(false);
     }
 
-    /* ---------------------------------------------------------- Form ---------------------------------------------------------- */
-
     private void initializeFields() {
-        first_name_text_field.setText(client.getFirstName());
-        last_name_text_field.setText(client.getLastName());
-        birthdate_text_field.setText(client.getBirthdate().toString());
-        nif_text_field.setText(client.getNif());
-        phone_text_field.setText(client.getPhone());
-        mobile_phone_text_field.setText(client.getMobilePhone());
-        profession_text_field.setText(client.getProfession());
-        email_text_field.setText(client.getEmailAddress());
+        firstNameTextField.setText(client.getFirstName());
+        lastNameTextField.setText(client.getLastName());
+        birthdateTextField.setText(client.getBirthdate().toString());
+        nifTextField.setText(client.getNif());
+        phoneTextField.setText(client.getPhone());
+        mobilePhoneTextField.setText(client.getMobilePhone());
+        professionTextField.setText(client.getProfession());
+        emailTextField.setText(client.getEmailAddress());
     }
 
     @FXML
-    private void onSaveChangesButtonClick(ActionEvent actionEvent) {
-        client.setFirstName(first_name_text_field.getText());
-        client.setLastName(last_name_text_field.getText());
-        client.setPhone(phone_text_field.getText());
-        client.setMobilePhone(mobile_phone_text_field.getText());
-        client.setProfession(profession_text_field.getText());
-        client.setEmailAddress(email_text_field.getText());
-        Bank.instance.updateClient(client);
-        feedback_message_label.setVisible(true);
-        feedback_message_label.setText("Your profile has been successfully updated");
-        try { App.changeScene(actionEvent, "/fxml/login.fxml"); }
-        catch (IOException exception) { throw new RuntimeException(exception); }
+    private void onSaveChangesButtonClick() {
+        updateClient();
+        showSuccessFeedback();
+    }
+
+    private void updateClient() {
+        client.setFirstName(firstNameTextField.getText());
+        client.setLastName(lastNameTextField.getText());
+        client.setPhone(phoneTextField.getText());
+        client.setMobilePhone(mobilePhoneTextField.getText());
+        client.setProfession(professionTextField.getText());
+        client.setEmailAddress(emailTextField.getText());
+        bank.updateClient(client);
+    }
+
+    private void showSuccessFeedback() {
+        feedbackMessageLabel.setVisible(true);
+        feedbackMessageLabel.setText("Your profile has been successfully updated");
     }
 }
