b9bdccdb7cdab5700876736c27aa5965a010ae03
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-11 21:27:34 +0000

    made this class more according to standars

--- Domain/src/main/java/com/rumos/rumosbank/domain/repositories/BankAccountRepository.java
+++ Domain/src/main/java/com/rumos/rumosbank/domain/repositories/BankAccountRepository.java
@@ -1,6 +1,7 @@
 package com.rumos.rumosbank.domain.repositories;
 
 import com.rumos.rumosbank.domain.models.BankAccount;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -15,15 +16,23 @@ public class BankAccountRepository {
     }
 
     public void insert(BankAccount bankAccount) {
-        entityManager.getTransaction().begin();
-        this.entityManager.persist(bankAccount);
-        entityManager.getTransaction().commit();
-        entityManager.close();
-        entityManagerFactory.close();
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(bankAccount);
+            entityManager.getTransaction().commit();
+        } catch (Exception exception) {
+            entityManager.getTransaction().rollback();
+            throw exception;
+        } finally {
+            entityManager.close();
+            entityManagerFactory.close();
+        }
     }
 
     public BankAccount get(String number) {
         String query = "SELECT b FROM BankAccount b WHERE b.number = :number";
-        return entityManager.createQuery(query, BankAccount.class).setParameter("number", number).getSingleResult();
+        return entityManager.createQuery(query, BankAccount.class)
+                .setParameter("number", number)
+                .getSingleResult();
     }
 }
