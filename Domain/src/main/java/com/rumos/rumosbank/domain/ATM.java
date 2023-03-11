2e237a6652b83c86183c11a0849ad8e212664603
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-11 21:27:56 +0000

    removed unnecessary code and am redoing the authenticate method

--- Domain/src/main/java/com/rumos/rumosbank/domain/ATM.java
+++ Domain/src/main/java/com/rumos/rumosbank/domain/ATM.java
@@ -1,4 +1,4 @@
-package com.rumos.rumosbank.domain.services;
+package com.rumos.rumosbank.domain;
 
 import com.rumos.rumosbank.domain.models.BankAccount;
 import com.rumos.rumosbank.domain.models.cards.Card;
@@ -8,38 +8,34 @@ import com.rumos.rumosbank.domain.repositories.CreditCardRepository;
 import com.rumos.rumosbank.domain.repositories.DebitCardRepository;
 import com.rumos.rumosbank.domain.repositories.DepositRepository;
 import com.rumos.rumosbank.domain.repositories.WithdrawRepository;
+
 import jakarta.persistence.NoResultException;
 
 import java.math.BigDecimal;
 
 public class ATM {
-    private static final int[] withdrawAmounts;
-    private static final int[] depositAmounts;
     public static final ATM instance;
 
     static {
-        withdrawAmounts = new int[] {10, 20, 40, 80, 100, 200, 400, 600, 800};
-        depositAmounts = new int[] {25, 50, 100, 250, 500, 1000, 2500, 5000, 10000};
         instance = new ATM(); }
+
+    public int[] getWithdrawAmounts() {
+        return new int[] {10, 20, 40, 80, 100, 200, 400, 600, 800};
+    }
+
+    public int[] getDepositAmounts() {
+        return new int[] {25, 50, 100, 250, 500, 1000, 2500, 5000, 10000};
+    }
 
     public Card authenticate(String cardNumber, String cardPin) {
         Card card;
-        try {
-            card = new DebitCardRepository().get(cardNumber);
+        card = new DebitCardRepository().get(cardNumber);
             if (card.isPinCorrect(cardPin)) return card;
-        }  catch (NoResultException exception) { System.out.println(exception.getMessage()); }
         try {
             card = new CreditCardRepository().get(cardNumber);
             if (card.isPinCorrect(cardPin)) return card;
         }  catch (NoResultException exception) { System.out.println(exception.getMessage()); }
         return null;
-    }
-
-    /* ------------------------------------------------------------ Withdraw ------------------------------------------------------------ */
-
-
-    public int[] getWithdrawAmounts() {
-        return withdrawAmounts;
     }
 
     public void makeWithdraw(BankAccount bankAccount, BigDecimal amount) throws IllegalArgumentException {
@@ -51,13 +47,6 @@ public class ATM {
         withdraw.setBankAccount(bankAccount);
         withdraw.setAmount(amount);
         new WithdrawRepository().insert(withdraw);
-    }
-
-    /* ------------------------------------------------------------ Deposit ------------------------------------------------------------ */
-
-
-    public int[] getDepositAmounts() {
-        return depositAmounts;
     }
 
     public void makeDeposit(BankAccount bankAccount, BigDecimal amount) {
