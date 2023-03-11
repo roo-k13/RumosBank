23646c2f4190e76b98749c00071019c4ee62cebd
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-11 09:29:16 +0000

    reorganized the class and refactored the onSavePinButtonClick() into smaller methods

--- ATMApp/src/main/java/com/rumos/rumosbank/atmapp/controllers/ChangePinController.java
+++ ATMApp/src/main/java/com/rumos/rumosbank/atmapp/controllers/ChangePinController.java
@@ -1,6 +1,9 @@
 package com.rumos.rumosbank.atmapp.controllers;
 
+import java.io.IOException;
+
 import com.rumos.rumosbank.atmapp.App;
+import com.rumos.rumosbank.domain.models.cards.Card;
 import com.rumos.rumosbank.domain.models.cards.CreditCard;
 import com.rumos.rumosbank.domain.models.cards.DebitCard;
 import com.rumos.rumosbank.domain.repositories.CreditCardRepository;
@@ -11,49 +14,68 @@ import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
-import java.io.IOException;
-import java.util.Objects;
-
 public class ChangePinController {
 
     @FXML
     private PasswordField insert_pin_text_field;
+
     @FXML
     private PasswordField confirm_pin_text_field;
+
     @FXML
     private TextField feedback_text_field;
-
-    @FXML
-    private void onSavePinButtonClick(ActionEvent event) {
-        if (areFieldsEmpty()) {
-            feedback_text_field.setText("Please fill both fields!");
-            return;
-        } else if (!doFieldsMatch()) {
-            feedback_text_field.setText("The passwords do not match");
-            return;
-        }
-
-        try {
-            App.getAuthenticatedCard().setPin(insert_pin_text_field.getText());
-            if (App.getAuthenticatedCard() instanceof DebitCard) {
-                new DebitCardRepository().update((DebitCard) App.getAuthenticatedCard());
-            } else if (App.getAuthenticatedCard() instanceof CreditCard) {
-                new CreditCardRepository().update((CreditCard) App.getAuthenticatedCard());
-            }
-            try {
-                App.changeScene(event, "/fxml/menu.fxml");
-            } catch (IOException exception) {
-                throw new RuntimeException(exception);
-            }
-        } catch (IllegalArgumentException exception) { feedback_text_field.setText(exception.getMessage());
-    }
-}
 
     private boolean areFieldsEmpty() {
         return insert_pin_text_field.getText().isEmpty() || confirm_pin_text_field.getText().isEmpty();
     }
 
     private boolean doFieldsMatch() {
-        return Objects.equals(insert_pin_text_field.getText(), confirm_pin_text_field.getText());
+        return insert_pin_text_field.getText().equals(confirm_pin_text_field.getText());
+    }
+
+    private void showFeedback(String message) {
+        feedback_text_field.setText(message);
+    }
+
+    @FXML
+    private void onSavePinButtonClick(ActionEvent event) {
+        if (areFieldsEmpty()) {
+            showFeedback("Please fill both fields!");
+            return;
+        }
+
+        if (!doFieldsMatch()) {
+            showFeedback("The passwords do not match");
+            return;
+        }
+
+        updatePin(event);
+    }
+
+    private void updatePin(ActionEvent event) {
+        try {
+            Card authenticatedCard = App.getAuthenticatedCard();
+            authenticatedCard.setPin(insert_pin_text_field.getText());
+            updateCardInRepository(authenticatedCard);
+            navigateToMenu(event);
+        } catch (IllegalArgumentException exception) {
+            showFeedback(exception.getMessage());
+        }
+    }
+
+    private void updateCardInRepository(Card card) {
+        if (card instanceof DebitCard) {
+            new DebitCardRepository().update((DebitCard) card);
+        } else if (card instanceof CreditCard) {
+            new CreditCardRepository().update((CreditCard) card);
+        }
+    }
+
+    private void navigateToMenu(ActionEvent event) {
+        try {
+            App.changeScene(event, "/fxml/menu.fxml");
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
     }
 }
