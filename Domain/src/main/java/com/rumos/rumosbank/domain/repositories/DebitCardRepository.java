f33b185e3b2a5e19e7d7d0ffb14e95b97e526902
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-11 23:30:37 +0000

    extended the AbstractRepository

--- Domain/src/main/java/com/rumos/rumosbank/domain/repositories/DebitCardRepository.java
+++ Domain/src/main/java/com/rumos/rumosbank/domain/repositories/DebitCardRepository.java
@@ -2,21 +2,10 @@ package com.rumos.rumosbank.domain.repositories;
 
 import com.rumos.rumosbank.domain.models.cards.DebitCard;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.NoResultException;
-import jakarta.persistence.Persistence;
 import jakarta.persistence.TypedQuery;
 
-public class DebitCardRepository {
-
-    private final EntityManager entityManager;
-    private final EntityManagerFactory entityManagerFactory;
-
-    public DebitCardRepository() {
-        entityManagerFactory = Persistence.createEntityManagerFactory("default");
-        entityManager = entityManagerFactory.createEntityManager();
-    }
+public class DebitCardRepository extends AbstractRepository<DebitCard> {
 
     public DebitCard getByNumber(String number) {
         String query = "SELECT d FROM DebitCard d WHERE d.number = :number";
@@ -28,26 +17,4 @@ public class DebitCardRepository {
             return null;
         }
     }
-
-    public void insert(DebitCard debitCard) {
-        try (EntityManager em = entityManagerFactory.createEntityManager()) {
-            em.getTransaction().begin();
-            em.persist(debitCard);
-            em.getTransaction().commit();
-        }
-    }
-
-    public void update(DebitCard debitCard) {
-        try (EntityManager em = entityManagerFactory.createEntityManager()) {
-            em.getTransaction().begin();
-            em.merge(debitCard);
-            em.getTransaction().commit();
-        }
-    }
-
-    public void close() {
-        entityManager.close();
-        entityManagerFactory.close();
-    }
-
 }
