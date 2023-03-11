f3be1cbd1746f4b5cdc6715074320f43afa3c983
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-11 09:49:05 +0000

    all the navigation methods are now in this class

--- ATMApp/src/main/java/com/rumos/rumosbank/atmapp/controllers/NavigationBarController.java
+++ ATMApp/src/main/java/com/rumos/rumosbank/atmapp/controllers/NavigationBarController.java
@@ -2,15 +2,36 @@ package com.rumos.rumosbank.atmapp.controllers;
 
 import com.rumos.rumosbank.atmapp.App;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 
 import java.io.IOException;
 
-public class NavigationBarController {
+public abstract class NavigationBarController {
 
     protected static void logout(ActionEvent actionEvent) {
         App.setAuthenticatedAccount(null);
         App.setAuthenticatedCard(null);
         try { App.changeScene(actionEvent, "/fxml/login.fxml"); }
+        catch (IOException exception) { throw new RuntimeException(exception); }
+    }
+
+    @FXML
+    private void onWithdrawButtonClick(ActionEvent actionEvent) {
+        OperationsController.selectedOperation = "Withdraw";
+        try { App.changeScene(actionEvent, "/fxml/operation.fxml"); }
+        catch (IOException exception) { throw new RuntimeException(exception); }
+    }
+
+    @FXML
+    private void onDepositButtonClick(ActionEvent actionEvent) {
+        OperationsController.selectedOperation = "Deposit";
+        try { App.changeScene(actionEvent, "/fxml/operation.fxml"); }
+        catch (IOException exception) { throw new RuntimeException(exception); }
+    }
+
+    @FXML
+    private void onChangePinButtonClick(ActionEvent actionEvent) {
+        try { App.changeScene(actionEvent, "/fxml/change_pin.fxml"); }
         catch (IOException exception) { throw new RuntimeException(exception); }
     }
 }
