2dfbf8433053eca16b7e5cd13a76e7252ef51966
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-11 15:39:08 +0000

    completed the tests for the setFirstName() and setLastName() methods

--- Domain/src/test/java/com/rumos/rumosbank/domain/models/ClientTest.java
+++ Domain/src/test/java/com/rumos/rumosbank/domain/models/ClientTest.java
@@ -3,26 +3,33 @@ package com.rumos.rumosbank.domain.models;
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class ClientTest {
+    private final Client client = new Client();
 
     @Test
-    void setFirstName() throws FileNotFoundException {
-        Client client = new Client();
-        try {
-            FileReader fileReader = new FileReader("src/test/java/com/rumos/rumosbank/domain/models/first_names.csv");
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                String name = line.trim();
-                System.out.println(name);
-                client.setFirstName(name);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    void setFirstName() {
+        testNames("src/test/java/com/rumos/rumosbank/domain/models/valid_first_names.csv");
+        assertThrows(IllegalArgumentException.class, ()
+                -> testNames("src/test/java/com/rumos/rumosbank/domain/models/invalid_first_names.csv"));
+    }
+
+    @Test
+    void setLastName() {
+        testNames("src/test/java/com/rumos/rumosbank/domain/models/valid_last_names.csv");
+        assertThrows(IllegalArgumentException.class, ()
+                -> testNames("src/test/java/com/rumos/rumosbank/domain/models/invalid_last_names.csv"));
+    }
+
+    private void testNames(String filePath) {
+        try (var reader = new BufferedReader(new FileReader(filePath))) {
+            reader.lines().map(String::trim).forEach(client::setFirstName);
+        } catch (IOException ioException) {
+            throw new RuntimeException(ioException);
         }
     }
 }
