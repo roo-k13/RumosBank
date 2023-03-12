afb4168784b839489e5be3e59443c049ca2bfc6a
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-12 16:39:31 +0000

    reorganized the class and added some validations

--- BankApp/src/main/java/com/rumos/rumosbank/bankapp/controllers/AccountController.java
+++ BankApp/src/main/java/com/rumos/rumosbank/bankapp/controllers/AccountController.java
@@ -1,5 +1,8 @@
 package com.rumos.rumosbank.bankapp.controllers;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import com.rumos.rumosbank.bankapp.App;
 import com.rumos.rumosbank.domain.models.BankAccount;
 import com.rumos.rumosbank.domain.models.movements.Movement;
@@ -9,36 +12,21 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 
-import java.math.BigDecimal;
-import java.util.List;
-
 public class AccountController extends NavigationBarController {
+
+    // Fields
     private BankAccount selectedAccount;
 
-    @FXML
-    private void initialize() {
-        initializeAccountsChoiceBox();
-        initializeMovementsTable();
-        feedback_message_label.setVisible(false);
-    }
-
-    /* --------------------------------------------------------- Accounts Choice Box ------------------------------------------------------- */
-
+    // JavaFX Controls
     @FXML
     private ChoiceBox<BankAccount> bank_accounts_choice_box;
-
-    @FXML
-    private void initializeAccountsChoiceBox() {
-        bank_accounts_choice_box.setOnAction((this::onSelectingBankAccount));
-        List<BankAccount> bankAccounts = App.getAuthenticatedClient().getBankAccounts();
-        bank_accounts_choice_box.setItems(FXCollections.observableList(bankAccounts));
-    }
-
-    /* --------------------------------------------------------- Movements Table --------------------------------------------------------- */
-
     @FXML
     private TableView<Movement> movements_table_view;
     @FXML
@@ -47,11 +35,25 @@ public class AccountController extends NavigationBarController {
     private TableColumn<Movement, String> movement_amount;
     @FXML
     private TableColumn<Movement, String> movement_date;
-
     @FXML
-    private void onSelectingBankAccount(ActionEvent actionEvent) {
-        selectedAccount = bank_accounts_choice_box.getValue();
-        updateMovements();
+    private TextField transfer_account_number_text_field;
+    @FXML
+    private TextField transfer_amount_text_field;
+    @FXML
+    private Label feedback_message_label;
+
+    // Initialization methods
+    @FXML
+    private void initialize() {
+        initializeAccountsChoiceBox();
+        initializeMovementsTable();
+        feedback_message_label.setVisible(false);
+    }
+
+    private void initializeAccountsChoiceBox() {
+        bank_accounts_choice_box.setOnAction(this::onSelectingBankAccount);
+        List<BankAccount> bankAccounts = App.getAuthenticatedClient().getBankAccounts();
+        bank_accounts_choice_box.setItems(FXCollections.observableList(bankAccounts));
     }
 
     private void initializeMovementsTable() {
@@ -60,28 +62,69 @@ public class AccountController extends NavigationBarController {
         movement_date.setCellValueFactory(new PropertyValueFactory<>("longDate"));
     }
 
-    private void updateMovements() {
-        ObservableList<Movement> movements = FXCollections.observableArrayList(selectedAccount.getMovements());
-        movements_table_view.setItems(movements);
+    // Event handler methods
+    @FXML
+    private void onSelectingBankAccount(ActionEvent actionEvent) {
+        selectedAccount = bank_accounts_choice_box.getValue();
+        updateMovements();
     }
-
-    /* ------------------------------------------------------------ Transfers ------------------------------------------------------------ */
-
-    @FXML
-    private TextField transfer_account_number_text_field;
-    @FXML
-    private TextField transfer_amount_text_field;
-    @FXML
-    private Label feedback_message_label;
 
     @FXML
     private void onMakeTransferButtonClick() {
-        BigDecimal amount = new BigDecimal(transfer_amount_text_field.getText());
-        String receiverAccountNumber = transfer_account_number_text_field.getText();
+        BigDecimal amount = getTransferAmount();
+        if (amount == null) {
+            feedback_message_label.setVisible(true);
+            feedback_message_label.setText("Invalid transfer amount.");
+            return;
+        }
+
+        String receiverAccountNumber = getReceiverAccountNumber();
+        if (receiverAccountNumber == null) {
+            feedback_message_label.setVisible(true);
+            feedback_message_label.setText("Receiver account number is required.");
+            return;
+        }
+
         try {
             Bank.instance.makeTransfer(selectedAccount, receiverAccountNumber, amount);
             Bank.instance.updateMovements(selectedAccount);
             updateMovements();
-        } catch (Exception exception) { feedback_message_label.setText(exception.getMessage()); }
+            transferSuccessful();
+        } catch (Exception exception) {
+            feedback_message_label.setText(exception.getMessage());
+        }
+    }
+
+    // Helper methods
+    private BigDecimal getTransferAmount() {
+        try {
+            BigDecimal amount = new BigDecimal(transfer_amount_text_field.getText());
+            if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+                throw new IllegalArgumentException("Transfer amount must be a positive number.");
+            }
+            return amount;
+        } catch (NumberFormatException exception) {
+            return null;
+        }
+    }
+
+    private String getReceiverAccountNumber() {
+        String receiverAccountNumber = transfer_account_number_text_field.getText().trim();
+        if (receiverAccountNumber.isEmpty()) {
+            return null;
+        }
+        return receiverAccountNumber;
+    }
+
+    private void transferSuccessful() {
+        feedback_message_label.setVisible(true);
+        feedback_message_label.setText("Transfer successful.");
+        transfer_amount_text_field.clear();
+        transfer_account_number_text_field.clear();
+    }
+
+    private void updateMovements() {
+        ObservableList<Movement> movements = FXCollections.observableArrayList(selectedAccount.getMovements());
+        movements_table_view.setItems(movements);
     }
 }
