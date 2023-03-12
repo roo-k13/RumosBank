3e3d43ff99a8650e694c9a01ec91d065174f4e18
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-12 11:47:16 +0000

    removed unnecessary code since this class now extends the abstract repository

--- Domain/src/main/java/com/rumos/rumosbank/domain/repositories/ClientRepository.java
+++ Domain/src/main/java/com/rumos/rumosbank/domain/repositories/ClientRepository.java
@@ -1,39 +1,16 @@
 package com.rumos.rumosbank.domain.repositories;
 
 import com.rumos.rumosbank.domain.models.Client;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
 
-public class ClientRepository {
-    private final EntityManager entityManager;
-    private final EntityManagerFactory entityManagerFactory;
+public class ClientRepository extends AbstractRepository<Client> {
 
-    public ClientRepository() {
-        entityManagerFactory = Persistence.createEntityManagerFactory("default");
-        entityManager = entityManagerFactory.createEntityManager();
-    }
-
-    public Client get(Long id) {
+    public Client getByEmail(Long id) {
         return entityManager.find(Client.class, id);
     }
 
-    public Client get(String email) {
-        String query = "SELECT c FROM Client c WHERE c.emailAddress = :email";
-        return entityManager.createQuery(query, Client.class).setParameter("email", email).getSingleResult();
-    }
-
-    public void insert(Client client) {
-        entityManager.getTransaction().begin();
-        this.entityManager.persist(client);
-        entityManager.getTransaction().commit();
-        entityManager.close();
-        entityManagerFactory.close();
-    }
-
-    public void update(Client client) {
-        entityManager.getTransaction().begin();
-        entityManager.merge(client);
-        entityManager.getTransaction().commit();
+    public Client getByEmail(String email) {
+        return entityManager.createNamedQuery("Client.getByEmail", Client.class)
+                .setParameter("email", email)
+                .getSingleResult();
     }
 }
