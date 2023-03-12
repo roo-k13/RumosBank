ca9bca9b639a10b0b2247c619706a57361a75dce
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-12 00:08:23 +0000

    added a new test class for the clients

--- Domain/src/test/java/com/rumos/rumosbank/domain/models/ClientTest.java
+++ Domain/src/test/java/com/rumos/rumosbank/domain/models/ClientTest.java
@@ -1,15 +1,53 @@
 package com.rumos.rumosbank.domain.models;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ClientTest {
-    private final Client client = new Client();
+    private Client client;
+
+    @BeforeEach
+    public void setUp() {
+        List<BankAccount> bankAccounts = new ArrayList<>();
+        bankAccounts.add(new BankAccount());
+        bankAccounts.add(new BankAccount());
+        bankAccounts.add(new BankAccount());
+
+        client = new Client();
+        client.setFirstName("John");
+        client.setLastName("Doe");
+        client.setBirthdate(LocalDate.of(1990, 1, 1));
+        client.setNif("123456789");
+        client.setPhone("123456789");
+        client.setMobilePhone("912345678");
+        client.setProfession("Developer");
+        client.setEmailAddress("john.doe@example.com");
+        client.setPassword("secret");
+        client.setBankAccounts(bankAccounts);
+    }
+
+    @Test
+    public void testGetName() {
+        assertEquals("John Doe", client.getName());
+    }
+
+    @Test
+    public void testGetBankAccount() {
+        BankAccount account = client.getBankAccount("001");
+        assertNotNull(account);
+        assertEquals("002", account.getNumber());
+        assertEquals(2000, account.getBalance());
+    }
+
 
     @Test
     void setFirstName() {
