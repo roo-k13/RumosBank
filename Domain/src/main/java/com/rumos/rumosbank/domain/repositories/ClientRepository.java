9fd3056a96308f9219363149619ea70e429e7371
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-13 07:55:16 +0000

    adjusted name of the methods and made them final

--- Domain/src/main/java/com/rumos/rumosbank/domain/repositories/ClientRepository.java
+++ Domain/src/main/java/com/rumos/rumosbank/domain/repositories/ClientRepository.java
@@ -4,11 +4,11 @@ import com.rumos.rumosbank.domain.models.Client;
 
 public class ClientRepository extends AbstractRepository<Client> {
 
-    public Client getByEmail(Long id) {
+    public final Client getById(Long id) {
         return getEntityManager().find(Client.class, id);
     }
 
-    public Client getByEmail(String email) {
+    public final Client getByEmail(String email) {
         return getEntityManager().createNamedQuery("Client.getByEmail", Client.class)
                 .setParameter("email", email)
                 .getSingleResult();
