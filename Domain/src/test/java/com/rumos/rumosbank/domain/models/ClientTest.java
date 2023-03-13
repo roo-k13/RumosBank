cc93e31eda2c8710ce39a7c4470216babf84e256
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-13 07:05:50 +0000

    corrected imports and made some methods final

--- Domain/src/test/java/com/rumos/rumosbank/domain/models/ClientTest.java
+++ Domain/src/test/java/com/rumos/rumosbank/domain/models/ClientTest.java
@@ -1,22 +1,22 @@
 package com.rumos.rumosbank.domain.models;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class ClientTest {
     private Client client;
 
     @BeforeEach
-    public void setUp() {
+    public final void setUp() {
         BankAccount bankAccount1 = new BankAccount();
         bankAccount1.setNumber("000000001");
 
@@ -26,7 +26,7 @@ class ClientTest {
         BankAccount bankAccount3 = new BankAccount();
         bankAccount3.setNumber("000000003");
 
-        List<BankAccount> bankAccounts = new ArrayList<>();
+        List<BankAccount> bankAccounts = new ArrayList<>(3);
         bankAccounts.add(bankAccount1);
         bankAccounts.add(bankAccount2);
         bankAccounts.add(bankAccount3);
@@ -45,34 +45,34 @@ class ClientTest {
     }
 
     @Test
-    public void testGetName() {
-        assertEquals("John Doe", client.getName());
+    public final void testGetName() {
+        Assertions.assertEquals("John Doe", client.getName(), "");
     }
 
     @Test
-    public void testGetBankAccount() {
+    public final void testGetBankAccount() {
         BankAccount account = client.getBankAccount("000000002");
-        assertNotNull(account);
-        assertEquals("000000002", account.getNumber());
+        Assertions.assertNotNull(account, "");
+        Assertions.assertEquals("000000002", account.getNumber(), "");
     }
 
 
     @Test
-    void setFirstName() {
+    final void setFirstName() {
         testNames("src/test/java/com/rumos/rumosbank/domain/models/valid_first_names.csv");
-        assertThrows(IllegalArgumentException.class, ()
+        Assertions.assertThrows(IllegalArgumentException.class, ()
                 -> testNames("src/test/java/com/rumos/rumosbank/domain/models/invalid_first_names.csv"));
     }
 
     @Test
-    void setLastName() {
+    final void setLastName() {
         testNames("src/test/java/com/rumos/rumosbank/domain/models/valid_last_names.csv");
-        assertThrows(IllegalArgumentException.class, ()
+        Assertions.assertThrows(IllegalArgumentException.class, ()
                 -> testNames("src/test/java/com/rumos/rumosbank/domain/models/invalid_last_names.csv"));
     }
 
     private void testNames(String filePath) {
-        try (var reader = new BufferedReader(new FileReader(filePath))) {
+        try (var reader = new BufferedReader(new FileReader(filePath, StandardCharsets.UTF_8))) {
             reader.lines().map(String::trim).forEach(client::setFirstName);
         } catch (IOException ioException) {
             throw new RuntimeException(ioException);
@@ -80,7 +80,7 @@ class ClientTest {
     }
 
     @Test
-    void testToString() {
+    final void testToString() {
         System.out.println(client);
     }
 }
