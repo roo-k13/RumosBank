ec45e2fe41db828b2c24a69696064b8936454925
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-13 01:37:49 +0000

    made method more abstract

--- Domain/src/main/java/com/rumos/rumosbank/domain/Generators.java
+++ Domain/src/main/java/com/rumos/rumosbank/domain/Generators.java
@@ -1,10 +1,11 @@
 package com.rumos.rumosbank.domain;
 
 import java.util.Random;
+import java.util.random.RandomGenerator;
 
 public class Generators {
     public String generateRandomNumber(int length) {
-        Random r = new Random();
+        RandomGenerator r = new Random();
         StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < length; i++) {
