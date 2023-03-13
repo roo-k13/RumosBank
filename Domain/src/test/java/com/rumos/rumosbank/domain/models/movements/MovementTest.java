f77e5834db5fa611ede112c15d30cd90c9097dea
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-13 00:24:48 +0000

    redid the test for the movement class

--- Domain/src/test/java/com/rumos/rumosbank/domain/models/movements/MovementTest.java
+++ Domain/src/test/java/com/rumos/rumosbank/domain/models/movements/MovementTest.java
@@ -1,13 +1,44 @@
 package com.rumos.rumosbank.domain.models.movements;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
 
 class MovementTest {
 
     @Test
-    void testToString() {
-        Deposit deposit = new Deposit();
-        deposit.setAmount(500);
-        System.out.println(deposit);
+    final void testSetAmountWithBigDecimal() {
+        Movement movement = new MovementImpl();
+        BigDecimal amount = new BigDecimal("100.00");
+        movement.setAmount(amount);
+        Assertions.assertEquals(amount, movement.getAmount(), "");
+    }
+
+    @Test
+    final void testSetAmountWithInt() {
+        Movement movement = new MovementImpl();
+        int amount = 100;
+        movement.setAmount(amount);
+        Assertions.assertEquals(new BigDecimal(amount), movement.getAmount(), "");
+    }
+
+    @Test
+    final void testSetAmountWithZero() {
+        Movement movement = new MovementImpl();
+        Assertions.assertThrows(IllegalArgumentException.class, () -> movement.setAmount(0));
+    }
+
+    @Test
+    final void testSetAmountWithNegativeValue() {
+        Movement movement = new MovementImpl();
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Integer amount = -100;
+            movement.setAmount(amount);
+        });
+    }
+
+    private static class MovementImpl extends Movement {
+        // Empty subclass for testing purposes
     }
 }
