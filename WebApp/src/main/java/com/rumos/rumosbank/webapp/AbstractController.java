34f4d672d38a339e3930a843398ceb15e312cd87
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-13 00:38:48 +0000

    renamed from main controller to abstract controller

---
+++ WebApp/src/main/java/com/rumos/rumosbank/webapp/AbstractController.java
@@ -0,0 +1,31 @@
+package com.rumos.rumosbank.webapp;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServlet;
+
+import java.io.IOException;
+
+abstract class AbstractController extends HttpServlet {
+    private static final String INDEX_PATH = "index.jsp";
+    private static final String ACCOUNT_PATH = "account.jsp";
+    static final String CLIENT = "client";
+
+    static void changeToIndex(ServletRequest request, ServletResponse response)
+            throws ServletException, IOException {
+        changePage(request, response, INDEX_PATH);
+    }
+
+    static void changeToAccount(ServletRequest request, ServletResponse response)
+            throws ServletException, IOException {
+        changePage(request, response, ACCOUNT_PATH);
+    }
+
+    private static void changePage(ServletRequest request, ServletResponse response, String path)
+            throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher(path);
+        dispatcher.forward(request, response);
+    }
+}
