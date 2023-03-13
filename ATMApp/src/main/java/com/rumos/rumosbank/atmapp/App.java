df5d0ca9bdd80d3f2b1f954555ffef5cbffa08d5
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-13 01:37:00 +0000

    extracted these methods to a new class

--- ATMApp/src/main/java/com/rumos/rumosbank/atmapp/App.java
+++ ATMApp/src/main/java/com/rumos/rumosbank/atmapp/App.java
@@ -1,20 +1,13 @@
 package com.rumos.rumosbank.atmapp;
 
-import com.rumos.rumosbank.domain.models.BankAccount;
-import com.rumos.rumosbank.domain.models.cards.Card;
-
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class App extends Application {
-    private static BankAccount authenticatedAccount;
-    private static Card authenticatedCard;
 
     public static void main(String[] args) {
         launch();
@@ -28,25 +21,5 @@ public class App extends Application {
         stage.setScene(scene);
         stage.show();
         stage.centerOnScreen();
-    }
-
-    /* ------------------------------------------------------------ Authenticated Account ------------------------------------------------------------ */
-
-    public static BankAccount getAuthenticatedAccount() {
-        return authenticatedAccount;
-    }
-
-    public static void setAuthenticatedAccount(BankAccount authenticatedAccount) {
-        App.authenticatedAccount = authenticatedAccount;
-    }
-
-    /* ------------------------------------------------------------ Authenticated Card ------------------------------------------------------------ */
-
-    public static Card getAuthenticatedCard() {
-        return authenticatedCard;
-    }
-
-    public static void setAuthenticatedCard(Card authenticatedCard) {
-        App.authenticatedCard = authenticatedCard;
     }
 }
