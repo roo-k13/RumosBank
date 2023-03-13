86c4a19215c20f431ea758028b45b905ba303b8a
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-13 01:26:16 +0000

    added the registration path

--- BankApp/src/main/java/com/rumos/rumosbank/bankapp/Paths.java
+++ BankApp/src/main/java/com/rumos/rumosbank/bankapp/Paths.java
@@ -1,5 +1,6 @@
 package com.rumos.rumosbank.bankapp;
 
-public class Paths {
-    public final static String LOGIN_PATH = "/fxml/login.fxml";
+public final class Paths {
+    public static final String LOGIN_PATH = "/fxml/login.fxml";
+    public static final String REGISTRATION_PATH = "/fxml/registration.fxml";
 }
